c77690839e14f9ffb6a650b584695dad39924858
kato-mahiro <kato.mahiro.3@gmail.com>
2025-02-23 11:05:34 +0900

    アプリを作りました

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Hello World")
+
+if 'counter' not in st.session_state:
+    st.session_state.counter = 0
+
+if st.button("Increment"):
+    st.session_state.counter += 1
+
+st.write(f"Counter: {st.session_state.counter}")
